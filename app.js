555edb2f0eda86584ebcfa7ca2ae484d5c57c4f0
prashantmaurya228 <34316534+prashantmaurya228@users.noreply.github.com>
2019-09-19 00:16:06 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
+//API key
 const key = 'c2678ede9c1a084281d9a64731d3f724';
-var cityName = document.querySelector('#cityName');
-const openWeather = async (cityName) => {
 
+var cityName = document.querySelector('#cityName');
+
+
+//API call
+const openWeather = async (cityName) => {
    var url = 'https://api.openweathermap.org/data/2.5/weather?q=';
    var query = `${cityName}&units=metric&APPID=${key}`;
    const response = await fetch(url + query);
    const data = await response.json();
-   	 updateUI(data); 
+   return data; 
 };
 
-
-function updateUI(data) {
+//updating UI
+ const updateUI = data => {
 	document.querySelector('#city-info').style.display="block";
+	document.querySelector('#error').style.display="none";
+
 //temperature
 	var temperature = document.querySelector('#temp');
 	temperature.innerHTML = data.main.temp;
+	
 
 //city name
 var name = document.querySelector('#city-name');
@@ -38,16 +45,24 @@ var humidity = document.querySelector('#humidity');
 
 }
 
+//updating UI when error found
+const errUpdate = () => {
+	document.querySelector('#city-info').style.display="none";
+	document.querySelector('#error').style.display="block";
+
+}
+
 
 city.addEventListener( 'submit' , e => {
 	e.preventDefault();
 
    var cityForm = document.querySelector('#cityName');
    cityName = cityForm.value.trim();
-    city.reset();
+   city.reset();
+
    openWeather(cityName)
-  
-  
+   .then(data => updateUI(data))
+   .catch( err =>errUpdate());
 });
 
 
